f0e781719268cccfc98513a2445a4ae8cb78e6b5
Yee Fei <yeefei@192.168.1.3>
2025-12-07 17:31:32 +0800

    Fix ECS service name to include environment suffix

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -164,7 +164,7 @@ resource "aws_ecs_task_definition" "app" {
 
 # ECS Service
 resource "aws_ecs_service" "app" {
-  name            = "${local.prefix}"
+  name            = local.prefix
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   desired_count   = 1
