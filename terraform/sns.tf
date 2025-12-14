37b3cc4f3f0077b3d210d44ee02966ac7b6f945d
Yee Fei <yeefei@192.168.1.3>
2025-12-14 15:06:56 +0800

    fix discord forwarder

--- terraform/sns.tf
+++ terraform/sns.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_permission" "sns_invoke_discord_forwarder" {
   count         = var.discord_webhook_url != "" ? 1 : 0
   statement_id  = "AllowExecutionFromSNS"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.discord_forwarder.function_name
+  function_name = aws_lambda_function.discord_forwarder[0].function_name
   principal     = "sns.amazonaws.com"
   source_arn    = aws_sns_topic.booking_notifications.arn
 }
@@ -20,7 +20,7 @@ resource "aws_sns_topic_subscription" "discord_forwarder" {
   count     = var.discord_webhook_url != "" ? 1 : 0
   topic_arn = aws_sns_topic.booking_notifications.arn
   protocol  = "lambda"
-  endpoint  = aws_lambda_function.discord_forwarder.arn
+  endpoint  = aws_lambda_function.discord_forwarder[0].arn
 
   depends_on = [aws_lambda_permission.sns_invoke_discord_forwarder]
 }
